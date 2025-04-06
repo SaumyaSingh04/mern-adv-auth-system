b2d27e736e70970169a3035877b79be7167887b3
SaumyaSingh04 <saumya0419@gmail.com>
2025-04-06 16:32:20 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import userRouter from './routes/user.routes.js';
+import authRoute from './routes/user.auth.js';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Setup __dirname for ES Module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Load environment variables
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+});
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRoute);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode: statusCode
+    });
+});
