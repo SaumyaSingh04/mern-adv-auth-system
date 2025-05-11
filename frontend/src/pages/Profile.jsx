1f9d29bec7a4005443734dc8e0490f1763603517
Saumya Singh <166956447+SaumyaSingh04@users.noreply.github.com>
2025-05-11 17:29:25 +0530

    Update Profile.jsx

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -66,7 +66,7 @@ export default function Profile  ()  {
     e.preventDefault()
     try{
       dispatch(updateUserStart())
-      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+      const res = await fetch(`${process.env.REACT_APP_API_URL}/api/user/update/${currentUser._id}`, {
         method: 'POST',
         headers: {
           'Content-Type' : 'application/json',
