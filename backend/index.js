41cdda10e32b81a8aabc9a5673726d969e5eefae
Saumya Singh <166956447+SaumyaSingh04@users.noreply.github.com>
2025-05-13 12:31:44 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'https://mern-adv-auth-system-motl.vercel.app',
+  origin: 'https://mern-adv-auth-system.vercel.app',
   credentials: true
 }));
 
